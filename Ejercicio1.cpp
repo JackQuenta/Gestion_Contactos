a588e08ab873f7b60c60cb8fe7e814234816702b
JackQuenta <jackalvaroqm@gmail.com>
2025-06-28 04:44:30 -0500

    Avance Ejercicio1 - Mostrando Enunciado, diseño del programa y agregación de estructura como pide en enunciado.

---
+++ Ejercicio1.cpp
@@ -0,0 +1,31 @@
+/*1. Gestión de Contactos
+Crear una estructura llamada contactoEmail que tenga las siguientes características: nombres completos, sexo, edad,
+teléfono, email, nacionalidad
+El programa debe constar de un menú que permita:
+a) Agregar un contacto
+b) Eliminar un contacto
+c) Mostrar listado general de contactos registrados hasta ese momento.
+d) Mostrar listado de contactos existentes, ordenado por servidor de correo de las cuentas (gmail.com, outlook.com,
+yahoo.com, etc.)
+e) Salir del programa
+El programa debe mantenerse en ejecución continuamente hasta que usuario indique que desea salir del mismo*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct contactoEmail {
+    string nombreCompleto;
+    char sexo;
+    int edad;
+    string telefono;
+    string email;
+    string nacionalidad;
+};
+
+int main() {
+    do {
+    } while();
+    
+    return 0;
+}
