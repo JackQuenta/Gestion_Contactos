7ad780d7320ec2d5fdc7b4cf7917c51e1ff06105
JackQuenta <jackalvaroqm@gmail.com>
2025-06-28 19:21:28 -0500

    Mejora y avance del Ejercicio1 - Se añade la función agregar contacto

--- Ejercicio1.cpp
+++ Ejercicio1.cpp
@@ -23,9 +23,57 @@ struct contactoEmail {
     string nacionalidad;
 };
 
+contactoEmail contactos[100];
+int numContactos = 0;
+
+void agregarContacto() {
+    cout << "\n=== AGREGAR CONTACTO ===" << endl;
+    cin.ignore();
+    cout << "Nombre completo: ";
+    getline(cin, contactos[numContactos].nombreCompleto);
+    
+    cout << "Sexo (m para masculino/f para femenino): ";
+    cin >> contactos[numContactos].sexo;
+    
+    cout << "Edad: ";
+    cin >> contactos[numContactos].edad;
+    
+    cin.ignore();
+    cout << "Telefono: ";
+    getline(cin, contactos[numContactos].telefono);
+    
+    cout << "Email: ";
+    getline(cin, contactos[numContactos].email);
+    
+    cout << "Nacionalidad: ";
+    getline(cin, contactos[numContactos].nacionalidad);
+    
+    numContactos++;
+    cout << "Contacto agregado exitosamente!" << endl;
+}
+
+
+
 int main() {
+	int salir = 0;
     do {
-    } while();
+        cout << "\n=== GESTION DE CONTACTOS ===" << endl;
+        cout << "a) Agregar un contacto" << endl; 
+		cout << "b) Salir del programa" << endl;
+		char op;
+        cin >> op;
+        
+        switch(op) {
+            case 'a':
+                agregarContacto();
+                break;
+            case 'b':
+                salir = 1;
+                break;
+            default:
+                cout << "Opcion no valida." << endl;
+        }
+    } while(salir == 0);
     
     return 0;
 }
